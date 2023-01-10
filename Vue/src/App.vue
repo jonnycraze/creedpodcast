52bb5b05b560185ddac4cfb1299a4d9623591b0a
Jon Best <jbest@immersiondata.com>
2023-01-10 05:28:10 -0600

    cleanup app showmore in vue

--- Vue/src/App.vue
+++ Vue/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-  import { ref } from 'vue';
+  import { reactive, ref } from 'vue';
   import data from './assets/data/podcasts-by-genre.json';
   import Header from './components/Header.vue';
   import Blurb from './components/Blurb.vue';
@@ -13,6 +13,9 @@
     data[1].podcasts[7],
   ]);
 
+  const state = reactive({
+    showMorePodcasts: false,
+  });
 </script>
 
 <template>
@@ -22,8 +25,11 @@
       <div v-for="(item, i) in podcasts" :key="i">
         <Blurb :data="item" :position="i+1" />
       </div>
+      <button data-testid="findMoreButton" class="showMoreButton" @click="state.showMorePodcasts = true">
+        Find More Podcasts
+      </button>
     </div>
-    <MorePodcasts v-if="data" :data="{data}" />
+    <MorePodcasts v-if="state.showMorePodcasts" :data="{data}" />
   </section>
 </template>
 
@@ -37,6 +43,24 @@
       max-width: 730px;
       margin: 0 auto $spacing-lg;
       padding: 15px 30px 15px 15px;
+  }
+  .showMoreButton {
+    cursor: pointer;
+    padding: $spacing-sm $spacing-md;
+    background-color: $color-lightPurple;
+    color: $color-white;
+    border: none;
+    font-size: 16px;
+    border-radius: 4px;
+    letter-spacing: .05rem;
+    text-transform: uppercase;
+    box-shadow: 2px 2px 4px rgba($color: #000000, $alpha: 0.3);
+    transition: all 0.5s;
+
+    &:hover {
+        background-color: $color-darkPurple;
+        transition: all 0.5s;
+    }
   }
 
 </style>
